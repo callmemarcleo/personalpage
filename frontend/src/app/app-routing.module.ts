ff3677f1b53d99aa4d5d8241a6b0e5a712b6eea0
callmemarcleo <marc.hoesli@students.bfh.ch>
2023-11-03 15:04:40 +0100

    Neue Components erstellt

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { InfoComponent } from './components/info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingpageComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'info', component: InfoComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { Router, RouterModule } from '@angular/router';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    ContactComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
