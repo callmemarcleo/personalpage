8d29120b2a25ed627e97fe12c9ac66d5b5e95dd6
callmemarcleo <marc.hoesli@students.bfh.ch>
2023-11-18 11:01:56 +0100

    Landingpage-switch to infopage

--- frontend/src/app/components/info/info.component.ts
+++ frontend/src/app/components/info/info.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  ngOnInit() {
+    const profileImage = document.getElementsByClassName('profile-image')[0] as HTMLImageElement;
+    const imageTitle = document.getElementsByClassName('image-title')[0] as HTMLParagraphElement;
+    const imageDescription = document.getElementsByClassName('image-description')[0] as HTMLParagraphElement;
+    const toggleButton = document.getElementsByClassName('toggle-button')[0] as HTMLButtonElement;
 
+    const images = [
+      { src: 'https://i.ibb.co/9gHsxQ7/rightnowpants.jpg', title: 'TODAY', description: 'Student BFH' },
+      { src: 'https://i.ibb.co/1XFSn1r/babypants.jpg', title: 'BACK IN THE DAYS', description: 'uncool since 2000' }
+    ];
+
+    let currentIndex = 0;
+
+    toggleButton.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % images.length;
+      updateImage();
+    });
+
+    function updateImage() {
+      profileImage.src = images[currentIndex].src;
+      imageTitle.textContent = images[currentIndex].title;
+      imageDescription.textContent = images[currentIndex].description;
+    }
+
+    // Initialisierung
+    updateImage();
+  }
 }
--- frontend/src/app/components/landingpage/landingpage.component.ts
+++ frontend/src/app/components/landingpage/landingpage.component.ts
@@ -1,38 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
   styleUrls: ['./landingpage.component.css']
 })
-export class LandingpageComponent implements OnInit {
-
-  ngOnInit() {
-    const profileImage = document.getElementsByClassName('profile-image')[0] as HTMLImageElement;
-    const imageTitle = document.getElementsByClassName('image-title')[0] as HTMLParagraphElement;
-    const imageDescription = document.getElementsByClassName('image-description')[0] as HTMLParagraphElement;
-    const toggleButton = document.getElementsByClassName('toggle-button')[0] as HTMLButtonElement;
-
-    const images = [
-      { src: 'https://i.ibb.co/9gHsxQ7/rightnowpants.jpg', title: 'TODAY', description: 'Student BFH' },
-      { src: 'https://i.ibb.co/1XFSn1r/babypants.jpg', title: 'BACK IN THE DAYS', description: 'uncool since 2000' }
-    ];
-
-    let currentIndex = 0;
-
-    toggleButton.addEventListener('click', () => {
-      currentIndex = (currentIndex + 1) % images.length;
-      updateImage();
-    });
-
-    function updateImage() {
-      profileImage.src = images[currentIndex].src;
-      imageTitle.textContent = images[currentIndex].title;
-      imageDescription.textContent = images[currentIndex].description;
-    }
-
-    // Initialisierung
-    updateImage();
-  }
+export class LandingpageComponent {
+  
 }
-
